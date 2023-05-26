d7004b33f9c9f51c2c2e127229f763bba5c56996
gaurav <gs_earchitect@hotmail.com>
2023-05-26 20:00:22 +0530

    party_seq_no in customer & supplier

--- src/main/java/main/Party_Main.java
+++ src/main/java/main/Party_Main.java
@@ -3,15 +3,17 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages ={"company","person","customer","supplier"})
-@EnableJpaRepositories(basePackages = {"company","person","customer","supplier"})
-@ComponentScan({"company","person","customer","supplier"})
+@EnableEurekaClient
+@EntityScan(basePackages ={"customer","party","enterprise","membership","person","supplier"})
+@EnableJpaRepositories(basePackages = {"customer","party","enterprise","membership","person","supplier"})
+@ComponentScan({"customer","party","enterprise","membership","person","supplier"})
 public class Party_Main extends SpringBootServletInitializer  
 {
 	
